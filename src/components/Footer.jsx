f2d337496bf8264ac08aa9b4aaf172d4210d03e9
Frank <frank_link_24@hotmail.com>
2025-08-15 20:16:56 +0200

    Changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
     <footer className="bg-yellow-400 text-blue-900 text-center p-6 mt-8">
-        <p>&copy; 2025 One Piece World. Todos los derechos reservados. Creado por un fan  <a className='border-solid shadow-2xl border-red-700 rounded-xl border-2 hover:text-yellow-400 hover:bg-slate-600 hover:shadow-red-950' target='_blank' href='https://curriculum-francisco-rodriguez.vercel.app/'>"Conóceme"</a></p>
+        <p>&copy; 2025 One Piece World. Todos los derechos reservados. Creado por un fan  <a className='border-solid shadow-2xl border-red-700 rounded-xl border-2 pr-1 pl-1 hover:text-yellow-400 hover:bg-slate-600 hover:shadow-red-950' target='_blank' href='https://curriculum-francisco-rodriguez.vercel.app/'>"Conóceme"</a></p>
     </footer>
     );
 };
